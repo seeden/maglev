cbc9c5073352800aa1080d5185ca1f84de5eaf5d
Zlatko Fedor <zfedor@gmail.com>
2016-02-18 00:35:36 +0100

    added middleware options

---
+++ src/constants/MiddlewareType.js
@@ -0,0 +1,7 @@
+import keymirror from 'keymirror';
+
+export default keymirror({
+  BEFORE_STATIC: null,
+  BEFORE_ROUTER: null,
+  AFTER_ROUTER: null,
+});
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Server from './server';
+import MiddlewareType from './constants/MiddlewareType';
+import controllers from './controllers';
+
+export { Server, MiddlewareType, controllers };
+export default Server;
